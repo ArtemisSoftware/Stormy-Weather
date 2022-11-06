f5006e10bb113efe4b3587fe29cc2e441c2d3e96
gustavo <gustavo.isaac@gmail.com>
2022-11-06 11:50:00 +0000

    WeatherViewModel

---
+++ app/src/main/java/com/artemissoftware/stormyweather/presentation/weather/WeatherViewModel.kt
@@ -0,0 +1,56 @@
+package com.artemissoftware.stormyweather.presentation.weather
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.artemissoftware.stormyweather.domain.repositories.LocationTrackerRepository
+import com.artemissoftware.stormyweather.domain.repositories.WeatherRepository
+import com.artemissoftware.stormyweather.domain.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherViewModel @Inject constructor(
+    private val weatherRepository: WeatherRepository,
+    private val locationTrackerRepository: LocationTrackerRepository
+): ViewModel() {
+
+    var state by mutableStateOf(WeatherState())
+        private set
+
+    fun loadWeatherInfo() {
+        viewModelScope.launch {
+            state = state.copy(
+                isLoading = true,
+                error = null
+            )
+            locationTrackerRepository.getCurrentLocation()?.let { location ->
+                when(val result = weatherRepository.getWeatherData(location.latitude, location.longitude)) {
+                    is Resource.Success -> {
+                        state = state.copy(
+                            weatherInfo = result.data,
+                            isLoading = false,
+                            error = null
+                        )
+                    }
+                    is Resource.Error -> {
+                        state = state.copy(
+                            weatherInfo = null,
+                            isLoading = false,
+                            error = result.message
+                        )
+                    }
+                }
+            } ?: kotlin.run {
+                state = state.copy(
+                    isLoading = false,
+                    error = "Couldn't retrieve location. Make sure to grant permission and enable GPS."
+                )
+            }
+        }
+    }
+}
