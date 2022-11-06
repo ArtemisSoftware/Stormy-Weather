ebbfba136fb7353a5b65d61dc18f686554756195
gustavo <gustavo.isaac@gmail.com>
2022-11-06 11:41:12 +0000

    StormyWeatherApplication

---
+++ app/src/main/java/com/artemissoftware/stormyweather/StormyWeatherApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.stormyweather
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StormyWeatherApplication : Application()
